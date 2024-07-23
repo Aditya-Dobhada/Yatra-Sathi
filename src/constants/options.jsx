2dd494a45f3de164e77ee3aa3f0e26714a401d79
Aditya Dobhada <adisb6262@gmail.com>
2024-07-24 04:38:44 +0530

    different travellers and budget options

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelsList = [
+  {
+    id: 1,
+    title: "Just Me",
+    desc: "A sole traveles in exploration",
+    icon: "✈︎",
+    people: "1 Person",
+  },
+  {
+    id: 2,
+    title: "A Couple",
+    desc: "Two traveles in tandem",
+    icon: "🥂",
+    people: "2 People",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "A group of fun loving loving adv",
+    icon: "🏡",
+    people: "3 to 5 People",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    desc: "A bunch of thrill-seekers",
+    icon: "⛵️",
+    people: "5 to 10 People",
+  },
+];
+
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Cheap",
+    desc: "Stay conscious of costs",
+    icon: "💵",
+  },
+  {
+    id: 2,
+    title: "Moderate",
+    desc: "Keep cost on the average side",
+    icon: "💰",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    desc: "Don't worry about the cost",
+    icon: "💸",
+  },
+];
+
+export const AI_PROMPT = "Generate Travel Plan for Location: {location}";
