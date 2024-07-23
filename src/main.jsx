a68f28fcd2cc13a7afe6df3b5545c962cd806f02
Aditya Dobhada <adisb6262@gmail.com>
2024-07-24 04:42:30 +0530

    fixed an issue with imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 import {
   BrowserRouter,
@@ -8,7 +8,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import CreateTrip from "./create-trip/index.jsx";
-import Header from "./components/custom/Header.tsx";
+import Header from "./components/custom/Header.jsx";
 
 const router = createBrowserRouter([
   {
