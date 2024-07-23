38e9db9d1375f7afcec19943b2eea9951506b1bf
Aditya Dobhada <adisb6262@gmail.com>
2024-07-24 04:39:47 +0530

    added routing with react router dom and added header file to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CreateTrip from "./create-trip/index.jsx";
+import Header from "./components/custom/Header.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
