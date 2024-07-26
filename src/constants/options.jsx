987734040413ab10d6d28688bd7060b4546bd2fc
Aditya Dobhada <adisb6262@gmail.com>
2024-07-26 23:07:04 +0530

    updated the AI prompt and added the photo reference url to fetch google places photos

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -50,4 +50,9 @@ export const SelectBudgetOptions = [
   },
 ];
 
-export const AI_PROMPT = "Generate Travel Plan for Location: {location}";
+export const AI_PROMPT =
+  "Generate Travel Plan for Location: {location} , for {noOfDays} Days for {travelers} with a {budget} budget ,Give me a Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates, ticket Pricing, Time travel each of the location for {noOfDays} days with each day plan with best time to visit in JSON format.Validate the output json, also the itinerary also needs to be of the type array just like hotels. in the itinerary, the Time means the time slot allocated to each place of travel";
+
+export const PHOTO_REF_URL =
+  "https://places.googleapis.com/v1/{NAME}/media?maxHeightPx=1080&maxWidthPx=1920&key=" +
+  import.meta.env.VITE_GOOGLE_PLACES_API_KEY;
